24f877f596ea64f20aca902722b6464a9a1d029a
Parker Smith <parkers@emergentsoftware.net>
2024-12-02 12:53:05 -0500

    Day 1

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+List<int> column1 = [];
+List<int> column2 = [];
+
+foreach (var row in input)
+{
+    var values = row.Split("   ");
+    column1.Add(int.Parse(values[0].Trim()));
+    column2.Add(int.Parse(values[1].Trim()));
+}
+
+column1.Sort();
+column2.Sort();
+
+int sum = 0;
+int similarity = 0;
+for (var i = 0; i < column1.Count; i++)
+{
+    var entry = column1[i];
+    sum += Math.Abs(entry - column2[i]);
+
+    similarity += entry * column2.Count(c => c == entry);
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(similarity);
