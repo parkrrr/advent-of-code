11d46f4f232be84103ccccf1473cb3f80c7a5c58
Parker Smith <parkerbsmith@gmail.com>
2024-12-03 23:56:00 -0500

    day 3

---
+++ 2024/Day3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var regex = new Regex("mul\\((\\d*),(\\d*)\\)|(don\\'t\\(\\))|(do\\(\\))");
+
+var accum = 0;
+
+var enabled = true;
+foreach (var memory in input)
+{
+    var instances = regex.Matches(memory);
+
+    foreach (Match instance in instances)
+    {
+        if (instance.Groups[0].Value == "don't()")
+        {
+            enabled = false;
+        }
+        else if (instance.Groups[0].Value == "do()")
+        {
+            enabled = true;
+        }
+        else
+        {
+            if (!enabled)
+            {
+                continue;
+            }
+
+            var a = int.Parse(instance.Groups[1].Value);
+            var b = int.Parse(instance.Groups[2].Value);
+
+            var result = a * b;
+
+            accum += result;
+        }
+    }
+}
+
+Console.WriteLine(accum);
